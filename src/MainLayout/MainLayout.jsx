5940680637dc30ccc9981a9de55b9a8f482f0928
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-02-21 23:32:34 +0600

    navbar

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
-    return (
-        <div className='max-w-7xl mx-auto px-2'>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="max-w-7xl mx-auto px-2">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
